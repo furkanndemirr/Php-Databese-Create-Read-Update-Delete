0fbaa58024a5a0b78482861cb41a7febf40fe26e
furkanndemirr <furkannndemirrr53@gmail.com>
2022-08-05 12:23:01 +0300

    Update index.php

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
 
 <div class="server">
     <img style="width: 200px;margin-left: 70px;padding-top: 20px" src="logo%20(1).png">
-    <form action="testpage.php" method="get">
+    <form action="testpage.php" method="post">
         <br><br><br>
         <td>Servername :
             <input id="control" type="text" name="sername">
